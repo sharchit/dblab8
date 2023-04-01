e625deed68d3a9d72646f498396a0e7e5f49ffd3
Archit Sharma <sh02archit@gmail.com>
2023-04-02 00:16:22 +0530

    Added screenshots

--- tbl.sql
+++ tbl.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id INT(11) NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
